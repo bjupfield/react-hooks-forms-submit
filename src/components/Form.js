df6c0b9e771bf03ce2d86eb146ea2a7b4eeef8de
Bennett <bjupfield@gmail.com>
2021-11-24 12:02:52 -0600

    did untill form created name list

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [suburaExtrema, setSuburaExtrema] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +13,36 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  function cronch(e){
+    e.preventDefault()
+    const wonderForm = {
+      firstName: firstName,
+      lastName: lastName,
+    };
+    const dataarr = [...suburaExtrema, wonderForm]
+    setSuburaExtrema(dataarr)
+    setFirstName("");
+    setLastName("");
+    console.log(suburaExtrema)
+  }
+
+  const mustSubmit = suburaExtrema.map((great, realestate) => {
+    return(
+      <div key = {realestate}>
+        {great.firstName} {great.lastName}
+      </div>
+    )
+  })
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={cronch}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {mustSubmit}
+    </div>
   );
 }
 
